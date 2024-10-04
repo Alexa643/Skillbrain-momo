a725b2ee34a6ccf4060c95f327f11881fc698328
Alexandra <anghelescgeorgiana242@gmail.com>
2024-10-04 12:49:20 +0300

    add sprint7

---
+++ sprint7/tema.js
@@ -0,0 +1,21 @@
+// Function that checks if a number is divisible by 10
+function isDivisibleBy10(number) {
+    return number % 10 === 0;
+}
+
+// Function that runs the program
+function runProgram() {
+    const number1 = 100;
+    const number2 = 23;
+
+    const result1 = isDivisibleBy10(number1);
+    const result2 = isDivisibleBy10(number2);
+
+    // Output: true
+    console.log(result1);
+
+    // Output: false
+    console.log(result2);
+    // Return a number instead of void
+    return 0; 
+}
