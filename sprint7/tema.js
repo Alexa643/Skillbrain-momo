abfaec01d4650372f930b875e86f643379dd1ef4
Alexandra <anghelescgeorgiana242@gmail.com>
2024-10-06 09:29:34 +0300

    add sprint7

--- sprint7/tema.js
+++ sprint7/tema.js
@@ -1,6 +1,10 @@
-// Function that checks if a number is divisible by 10
+// Function that checks if a number is divisible by 10 using if-else
 function isDivisibleBy10(number) {
-    return number % 10 === 0;
+    if (number % 10 === 0) {
+        return true;  // Return true if divisible by 10
+    } else {
+        return false; // Return false otherwise
+    }
 }
 
 // Function that runs the program
@@ -16,6 +20,55 @@ function runProgram() {
 
     // Output: false
     console.log(result2);
-    // Return a number instead of void
-    return 0; 
+
+    return 0; // Return a number instead of void
 }
+
+// Function that takes 4 arguments and displays the fortune
+function tellFortune(numberOfChildren, partnerName, geographicLocation, jobTitle) {
+    // Using template literals to format the output
+    console.log(`You will be a ${jobTitle} in ${geographicLocation}, married to ${partnerName} and you will have ${numberOfChildren} children.`);
+}
+
+// Function that returns the day of the week based on the input number (in English)
+function getDayOfWeek(dayNumber) {
+    let dayName;
+    switch (dayNumber) {
+        case 1:
+            dayName = "Monday";
+            break;
+        case 2:
+            dayName = "Tuesday";
+            break;
+        case 3:
+            dayName = "Wednesday";
+            break;
+        case 4:
+            dayName = "Thursday";
+            break;
+        case 5:
+            dayName = "Friday";
+            break;
+        case 6:
+            dayName = "Saturday";
+            break;
+        case 7:
+            dayName = "Sunday";
+            break;
+        default:
+            dayName = "Invalid day number";
+    }
+    return dayName;
+}
+
+// Call the functions to execute the program
+runProgram();
+tellFortune(2, "Daniel", "Germany", "Programmer");
+
+// Get the day of the week based on input number (in English)
+
+// Output: Monday
+console.log(getDayOfWeek(1));
+
+// Output: Friday 
+console.log(getDayOfWeek(5)); 
