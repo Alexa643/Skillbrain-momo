1472f37412efc5736722a82310e8d845c9b160db
Alexandra <anghelescgeorgiana242@gmail.com>
2025-01-20 20:02:43 +0200

    am sters useEffect

--- sprint16-react-state/homework/4.3. todo-app/src/components/checkbox/CheckBox.jsx
+++ sprint16-react-state/homework/4.3. todo-app/src/components/checkbox/CheckBox.jsx
@@ -1,24 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./Checkbox.css";
 
 const Checkbox = (props) => {
   const { checked: propChecked, onChange } = props;
   const [checked, setChecked] = useState(!!propChecked);
 
-  useEffect(() => {
-    console.log("Checkbox updated:", checked);
-    if (onChange) {
-      onChange(checked);
-    }
-  }, [checked, onChange]);
-
-  useEffect(() => {
-    console.log("Checkbox propChecked changed:", propChecked);
-    setChecked(!!propChecked);
-  }, [propChecked]);
-
   const handleCheckboxChange = (e) => {
-    setChecked(e.target.checked);
+    const isChecked = e.target.checked;
+    setChecked(isChecked);
+    if (onChange) {
+      onChange(isChecked);
+    }
   };
 
   return (
